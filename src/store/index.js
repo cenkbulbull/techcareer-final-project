b5a40f565015d1c79bf7f2e3fde4798f8da6a56a
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-04-02 16:36:11 +0300

    The project framework was created

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const useProducts = defineStore('products', {
+  state: () => ({
+    products: []
+  }),
+  getters: {
+    getAllProducts(state){
+
+    }
+  },
+  actions: {
+    
+  },
+})
