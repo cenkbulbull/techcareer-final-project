e488f476dae3afd809c344ff752e581dd1ff2f6d
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-04-07 04:04:09 +0300

    High-scoring products were shown on the slider

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
-    <ProductsDataView/>
+  <HighestRatedCarousel />
+  <div>
+    <h5 class="ml-7">Tüm Ürünler</h5>
+    <ProductsDataView />
+  </div>
 </template>
 <script setup>
-import ProductsDataView from '../components/ProductsDataView.vue';
+import ProductsDataView from "../components/ProductsDataView.vue";
+import HighestRatedCarousel from "../components/HighestRatedCarousel.vue";
 </script>
